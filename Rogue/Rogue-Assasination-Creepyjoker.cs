3f96b20393653fe84354b4e2ffb152df32ecd5d6
CreepyJoker <noaxeqtr@gmail.com>
2017-02-10 16:43:41 +0200

    Updated UseCooldowns/Removed IsBoss functions.
    Removed IsBoss functions;
    Added the toggle button ( i've forgot to add it yesterday ).

--- Rogue/Rogue-Assasination-Creepyjoker.cs
+++ Rogue/Rogue-Assasination-Creepyjoker.cs
@@ -1,10 +1,20 @@
-// winifix@gmail.com
 // ReSharper disable UnusedMember.Global
 
 
-using System.Drawing;
-using System.Windows.Forms;
 using PixelMagic.Helpers;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using PixelMagic.GUI;
 
 namespace PixelMagic.Rotation
 {
@@ -25,27 +35,42 @@ namespace PixelMagic.Rotation
 
         public override void Initialize()
         {
-            Log.Write("Welcome to Rogue Assasination by Creepyjoker.", Color.Green);
-			Log.Write("The following rotation is still in beta version.", Color.Red);
-            Log.Write("Suggested build: Master Poisoner - Vigor - AP");
+            Log.Write("Welcome to Rogue-Assassination", Color.Green);
+            Log.Write("Suggested build : MP - Vigor - AP");
         }
 
         public override void Stop()
         {
         }
 
-
-        public override void Pulse()
+       private readonly Stopwatch stopwatch = new Stopwatch();
+       public override void Pulse()
         {
-            if (combatRoutine.Type == RotationType.SingleTarget) // Do Single Target Stuff here
+			if (stopwatch.ElapsedMilliseconds == 0)
+                    {
+						stopwatch.Start ();
+						Log.WritePixelMagic("The Cooldown toggle button is now Active (Numpad0). The delay is set to 1000ms ( 1 second )", Color.Black);	
+						return;
+					}		
+            {
+			if (DetectKeyPress.GetKeyState(DetectKeyPress.VK_NUMPAD0) < 0)
+            {
+                if(stopwatch.ElapsedMilliseconds > 1000)
+                { 
+                    combatRoutine.UseCooldowns = !combatRoutine.UseCooldowns;
+                    WoW.Speak("Cooldowns "+ (combatRoutine.UseCooldowns ? "On" : "Off"));
+                    stopwatch.Restart();
+                }
+            }
+            if (combatRoutine.Type == RotationType.SingleTarget)
             {
 				
-				if ( WoW.HasTarget && WoW.IsSpellInRange("Rupture"))
+				if (WoW.IsSpellInRange("Rupture") && WoW.IsInCombat)
 					
 				
 				{
-					if ( WoW.HasBossTarget &&
-                        WoW.CanCast("Kingsbane") &&
+					if ( UseCooldowns &&
+                        WoW.CanCast("Kingsbane") && WoW.CurrentComboPoints <= 4 && WoW.Energy >= 35 &&
                         !WoW.IsSpellOnCooldown("Kingsbane") &&
                             (
                             WoW.SpellCooldownTimeRemaining("Vendetta") >= 10 ||
@@ -55,47 +80,62 @@ namespace PixelMagic.Rotation
                         WoW.CastSpell("Kingsbane");
                         return;
                     }
-					if (WoW.CanCast("Garrote") && WoW.Energy >= 45 && !WoW.TargetHasDebuff("Garrote") && !WoW.IsSpellOnCooldown("Garrote") && WoW.CurrentComboPoints <= 4)
+					if (!WoW.PlayerHasBuff("Vanish") && WoW.CanCast("Garrote") && WoW.Energy >= 45 && !WoW.TargetHasDebuff("Garrote") && !WoW.IsSpellOnCooldown("Garrote") && WoW.CurrentComboPoints <= 4 && WoW.IsSpellInRange("Garrote"))
 					{
 						WoW.CastSpell("Garrote");
 						return;
 					}
-					if (WoW.CanCast("Fan Of Knives") && WoW.Energy >= 35 && WoW.PlayerHasBuff("FoK") && WoW.PlayerBuffStacks("FoK") == 30 && WoW.CurrentComboPoints <= 4)
+					if (WoW.TargetHasDebuff("Vendetta") && WoW.CanCast("Fan Of Knives") && WoW.Energy >= 35 && WoW.PlayerHasBuff("FoK") && WoW.PlayerBuffStacks("FoK") == 30 && WoW.CurrentComboPoints <= 4)
 					{
 						WoW.CastSpell("Fan Of Knives");
 						return;	
 					}
-					if (WoW.CanCast("Garrote") && WoW.Energy >= 45 && WoW.TargetHasDebuff("Garrote") && WoW.TargetDebuffTimeRemaining("Garrote") <= 4 && WoW.CurrentComboPoints <= 4)
+					if (!WoW.PlayerHasBuff("Vanish") && WoW.CanCast("Garrote") && WoW.Energy >= 45 && WoW.TargetHasDebuff("Garrote") && WoW.TargetDebuffTimeRemaining("Garrote") <= 3 && WoW.CurrentComboPoints <= 4 && WoW.IsSpellInRange("Garrote"))
 					{
 						WoW.CastSpell("Garrote");
 						return;
 					}
-					if (WoW.CurrentComboPoints == 4 && WoW.Energy >= 25 && WoW.CanCast("Rupture") && !WoW.TargetHasDebuff("Rupture"))
+					if (WoW.CurrentComboPoints == 4 && WoW.Energy >= 25 && WoW.CanCast("Rupture") && !WoW.TargetHasDebuff("Rupture") && WoW.IsSpellInRange("Garrote"))
 					{
 						WoW.CastSpell("Rupture");
 						return;
 					}
-					if (WoW.CurrentComboPoints == 4 && WoW.Energy >= 25 && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") <= 6)
+					if (WoW.CurrentComboPoints == 4 && WoW.Energy >= 25 && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") <= 6 && WoW.IsSpellInRange("Garrote"))
 					{
 						WoW.CastSpell("Rupture");
 						return;
 					}
-					if (WoW.CurrentComboPoints == 5 && WoW.Energy >= 25 && WoW.CanCast("Rupture") && !WoW.TargetHasDebuff("Rupture"))
+					if (WoW.CurrentComboPoints == 5 && WoW.Energy >= 25 && WoW.CanCast("Rupture") && !WoW.TargetHasDebuff("Rupture") && WoW.IsSpellInRange("Garrote"))
 					{
 						WoW.CastSpell("Rupture");
 						return;
 					}
-					if (WoW.CurrentComboPoints == 5 && WoW.Energy >= 25 && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") <= 6)
+					if (WoW.CurrentComboPoints == 5 && WoW.Energy >= 25 && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") <= 6 && WoW.IsSpellInRange("Garrote"))
 					{
 						WoW.CastSpell("Rupture");
 						return;
 					}
-					if (WoW.Energy >= 35 && WoW.CurrentComboPoints == 4 && WoW.CanCast("Envenom") && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") > 6)
+					if (!WoW.PlayerHasBuff("Vanish") && WoW.TargetHasDebuff("Toxins") && WoW.TargetDebuffTimeRemaining("Toxins") <= 1.5 && WoW.Energy >= 35 && WoW.CurrentComboPoints == 4 && WoW.CanCast("Envenom") && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") > 6 && WoW.IsSpellInRange("Garrote"))
 					{
 						WoW.CastSpell("Envenom");
 						return;
 					}
-					if (WoW.Energy >= 35 && WoW.CurrentComboPoints == 5 && WoW.CanCast("Envenom") && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") > 6)
+					if (!WoW.PlayerHasBuff("Vanish") && WoW.TargetHasDebuff("Toxins") && WoW.TargetDebuffTimeRemaining("Toxins") <= 1.5 && WoW.Energy >= 35 && WoW.CurrentComboPoints == 5 && WoW.CanCast("Envenom") && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") > 6 && WoW.IsSpellInRange("Garrote"))
+					{
+						WoW.CastSpell("Envenom");
+						return;
+					}
+					if (!WoW.PlayerHasBuff("Vanish") && !WoW.TargetHasDebuff("Toxins") && WoW.Energy >= 35 && WoW.CurrentComboPoints == 4 && WoW.CanCast("Envenom") && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") > 6 && WoW.IsSpellInRange("Garrote"))
+					{
+						WoW.CastSpell("Envenom");
+						return;
+					}
+					if (!WoW.PlayerHasBuff("Vanish") && !WoW.TargetHasDebuff("Toxins") && WoW.Energy >= 35 && WoW.CurrentComboPoints == 5 && WoW.CanCast("Envenom") && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") > 6 && WoW.IsSpellInRange("Garrote"))
+					{
+						WoW.CastSpell("Envenom");
+						return;
+					}
+					if (!WoW.PlayerHasBuff("Vanish") && WoW.TargetHasDebuff("Toxins") && WoW.TargetHasDebuff("Vendetta") && WoW.Energy >= 140 && WoW.CurrentComboPoints >= 4 && WoW.CanCast("Envenom") && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") > 6 && WoW.IsSpellInRange("Garrote"))
 					{
 						WoW.CastSpell("Envenom");
 						return;
@@ -104,146 +144,90 @@ namespace PixelMagic.Rotation
 					{
 						WoW.CastSpell("Mutilate");
 						return;
-					}
-					
-					if (WoW.HasBossTarget && !WoW.IsSpellOnCooldown("Kingsbane") && WoW.CanCast("Kingsbane") && WoW.CurrentComboPoints <= 4 && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") <= 10 && WoW.Energy >= 35)
-					{
-						WoW.CastSpell("Kingsbane");
-						return;
-					}
-					if (WoW.HasBossTarget && !WoW.IsSpellOnCooldown("Kingsbane") && WoW.CanCast("Kingsbane") && WoW.CurrentComboPoints <= 4 && WoW.TargetHasDebuff("Garrote") && WoW.TargetDebuffTimeRemaining("Garrote") <= 10 && WoW.Energy >= 35)
-					{
-						WoW.CastSpell("Kingsbane");
-						return;
-					}
-					if (WoW.HasBossTarget && !WoW.IsSpellOnCooldown("Kingsbane") && WoW.CanCast("Kingsbane") && WoW.CurrentComboPoints <= 4 && !WoW.TargetHasDebuff("Rupture") && WoW.Energy >= 35 )
-					{
-						WoW.CastSpell("Kingsbane");
-						return;
-					}
-                         
- 					if (
-                       WoW.HasBossTarget && WoW.CanCast("Vendetta") &&
+					}					
+ 					if (UseCooldowns && WoW.CanCast("Vendetta") &&
                         !WoW.IsSpellOnCooldown("Vendetta") &&
                         WoW.Energy <= 50
                         ) {
                         WoW.CastSpell("Vendetta");
                         return;
                     }
-					/*if (WoW.HasBossTarget && WoW.CanCast("Vanish") && !WoW.IsSpellOnCooldown("Vanish") && WoW.CurrentComboPoints == 5 && WoW.Energy >= 35 && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") <= 6)
+					/*if (UseCooldowns &&
+                        WoW.CanCast("Vanish") &&
+                        WoW.CurrentComboPoints >= 5 &&
+                        WoW.Energy >= 25 && WoW.TargetHasDebuff("Vendetta") &&
+                        (
+                            WoW.TargetHasDebuff("Rupture") &&
+                            WoW.TargetDebuffTimeRemaining("Rupture") < 10
+                        )
+                    ) {
+                        WoW.CastSpell("Vanish");
+                        return;
+                    }*/
+					if (UseCooldowns && WoW.CurrentComboPoints == 5 && !WoW.IsSpellOnCooldown("Vanish") && WoW.Energy >= 35 && WoW.TargetHasDebuff("Vendetta") )
 					{
 						WoW.CastSpell("Vanish");
 						return;
-					}*/
-						
-					
-					
-					
+					}
+					if (UseCooldowns && WoW.CurrentComboPoints == 5 && WoW.PlayerHasBuff("Vanish") && WoW.Energy >= 35 && WoW.TargetHasDebuff("Vendetta"))
+					{
+						WoW.CastSpell("Rupture");
+						return;
+					}
 					
 				}
                 
-                }
+            }
             
 
             if (combatRoutine.Type == RotationType.AOE || combatRoutine.Type == RotationType.SingleTargetCleave) // Do AoE Target Stuff here
             {
-                if ((WoW.HasTarget || WoW.IsBoss) && WoW.TargetIsEnemy && WoW.IsInCombat) {
-                   {
-					
-					if (WoW.CanCast("Garrote") && WoW.Energy >= 45 && !WoW.TargetHasDebuff("Garrote") && !WoW.IsSpellOnCooldown("Garrote") && WoW.CurrentComboPoints <= 4)
-					{
-						WoW.CastSpell("Garrote");
-						return;
-					}
-					if (WoW.CanCast("Garrote") && WoW.Energy >= 45 && WoW.TargetHasDebuff("Garrote") && WoW.TargetDebuffTimeRemaining("Garrote") <= 3 && WoW.CurrentComboPoints <= 4)
-					{
-						WoW.CastSpell("Garrote");
-						return;
-					}
-					if (WoW.CurrentComboPoints == 4 && WoW.Energy >= 25 && WoW.CanCast("Rupture") && !WoW.TargetHasDebuff("Rupture"))
-					{
-						WoW.CastSpell("Rupture");
-						return;
-					}
-					if (WoW.CurrentComboPoints == 4 && WoW.Energy >= 25 && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") <= 6)
-					{
-						WoW.CastSpell("Rupture");
-						return;
-					}
-					if (WoW.CurrentComboPoints == 5 && WoW.Energy >= 25 && WoW.CanCast("Rupture") && !WoW.TargetHasDebuff("Rupture"))
-					{
-						WoW.CastSpell("Rupture");
-						return;
-					}
-					if (WoW.CurrentComboPoints == 5 && WoW.Energy >= 25 && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") <= 6)
-					{
-						WoW.CastSpell("Rupture");
-						return;
-					}
-					if (WoW.Energy >= 35 && WoW.CurrentComboPoints == 4 && WoW.CanCast("Envenom") && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") > 6)
-					{
-						WoW.CastSpell("Envenom");
-						return;
-					}
-					if (WoW.Energy >= 35 && WoW.CurrentComboPoints == 5 && WoW.CanCast("Envenom") && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") > 6)
-					{
-						WoW.CastSpell("Envenom");
-						return;
-					}
-					if (WoW.CanCast("Fan Of Knives") && WoW.Energy >= 35 && WoW.CurrentComboPoints <= 3 )
+                if (WoW.HasTarget && WoW.IsSpellInRange("Rupture") && WoW.IsInCombat)
+				{
+					if (WoW.Energy >= 35 && WoW.CurrentComboPoints <= 4 && WoW.CanCast("Fan Of Knives"))
 					{
 						WoW.CastSpell("Fan Of Knives");
 						return;
 					}
-					if ( WoW.HasBossTarget &&
-                        WoW.CanCast("Kingsbane") &&
-                        !WoW.IsSpellOnCooldown("Kingsbane") &&
-                            (
-                            WoW.SpellCooldownTimeRemaining("Vendetta") >= 10 ||
-                            WoW.TargetHasDebuff("Vendetta")
-                            ))
-                         {
-                        WoW.CastSpell("Kingsbane");
-                        return;
-                    }
-					if (WoW.HasBossTarget && !WoW.IsSpellOnCooldown("Kingsbane") && WoW.CanCast("Kingsbane") && WoW.CurrentComboPoints <= 4 && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") <= 10 && WoW.Energy >= 35)
+					if (WoW.Energy >= 35 && WoW.CurrentComboPoints == 4 && WoW.TargetHealthPercent <= 35 && WoW.CanCast("Envenom"))
 					{
-						WoW.CastSpell("Kingsbane");
+						WoW.CastSpell("Envenom");
 						return;
 					}
-					if (WoW.HasBossTarget && !WoW.IsSpellOnCooldown("Kingsbane") && WoW.CanCast("Kingsbane") && WoW.CurrentComboPoints <= 4 && WoW.TargetHasDebuff("Garrote") && WoW.TargetDebuffTimeRemaining("Garrote") <= 10 && WoW.Energy >= 35)
+					if (WoW.Energy >= 35 && WoW.CurrentComboPoints == 5 && WoW.TargetHealthPercent <= 35 && WoW.CanCast("Envenom"))
 					{
-						WoW.CastSpell("Kingsbane");
+						WoW.CastSpell("Envenom");
 						return;
 					}
-					if (WoW.HasBossTarget && !WoW.IsSpellOnCooldown("Kingsbane") && WoW.CanCast("Kingsbane") && WoW.CurrentComboPoints <= 4 && !WoW.TargetHasDebuff("Rupture") && WoW.Energy >= 35 )
+					if (WoW.Energy >= 25 && WoW.CurrentComboPoints == 4 && WoW.TargetHealthPercent >= 36 && WoW.CanCast("Rupture") && !WoW.TargetHasDebuff("Rupture"))
 					{
-						WoW.CastSpell("Kingsbane");
+						WoW.CastSpell("Rupture");
 						return;
 					}
-                         
- 					if (
-                       WoW.HasBossTarget && WoW.CanCast("Vendetta") &&
-                        !WoW.IsSpellOnCooldown("Vendetta") &&
-                        WoW.Energy <= 50
-                        ) {
-                        WoW.CastSpell("Vendetta");
-                        return;
-                    }
-					if (WoW.HasBossTarget && WoW.CanCast("Vanish") && !WoW.IsSpellOnCooldown("Vanish") && WoW.CurrentComboPoints == 5 && WoW.Energy >= 35 && WoW.TargetHasDebuff("Rupture") && WoW.TargetDebuffTimeRemaining("Rupture") <= 6 && WoW.TargetDebuffTimeRemaining("Garrote") >= 6)
+					if (WoW.Energy >= 25 && WoW.CurrentComboPoints == 5 && WoW.TargetHealthPercent >= 36 && WoW.CanCast("Rupture") && !WoW.TargetHasDebuff("Rupture"))
 					{
-						WoW.CastSpell("Vanish");
+						WoW.CastSpell("Rupture");
 						return;
 					}
+					if (WoW.Energy >= 35 && WoW.CurrentComboPoints >= 4 && WoW.TargetHealthPercent >= 36 && WoW.TargetHasDebuff("Rupture") && WoW.CanCast("Envenom"))
+					{
+						WoW.CastSpell("Envenom");
+						return;
+					}
+					if (WoW.CanCast("Garrote") && WoW.Energy >= 45 && !WoW.TargetHasDebuff("Garrote") && !WoW.IsSpellOnCooldown("Garrote") && WoW.CurrentComboPoints <= 4 && WoW.IsSpellInRange("Garrote"))
+					{
+						WoW.CastSpell("Garrote");
+						return;
+					}
+				}
             }
         }
     }
 }
-	}
 }
 /*
 [AddonDetails.db]
-AddonAuthor=Vectarius, Dubsys
+AddonAuthor=CreepyJoker
 AddonName=Sucstobeyou
 WoWVersion=Legion - 70100
 [SpellBook.db]
@@ -259,8 +243,11 @@ Aura,1943,Rupture
 Aura,1784,Stealth
 Aura,703,Garrote
 Aura,208693,FoK
+Aura,1856,Vanish
+Aura,192425,Toxins
 Aura,32645,Envenom
 Aura,200802,Agonizing Poison
 Aura,193641,Elaborate Planning
 Aura,79140,Vendetta
 */
+
