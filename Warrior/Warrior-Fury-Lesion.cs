a2e77716f53a9aeb1af8dbb3ca15e8daed055e30
myrex1 <m.y.x@live.co.uk>
2017-02-23 20:05:12 +0000

    Tweaked FrothZerk RageBlow & Execute phase

--- Warrior/Warrior-Fury-Lesion.cs
+++ Warrior/Warrior-Fury-Lesion.cs
@@ -15,7 +15,8 @@ namespace PixelMagic.Rotation
 {
     public class FuryWarrior : CombatRoutine
     {
-		
+		//will check a list of spells and interrupt ones deemed important by wowhead guides
+        private CheckBox mplusint;		
 		private CheckBox TrueFrothingZerkerFalseMassacre;
 		private CheckBox MplusAoE;
 		
@@ -47,7 +48,16 @@ namespace PixelMagic.Rotation
             }
             set { ConfigFile.WriteValue("FuryWarrior", "MplusAoE", value.ToString()); }
         }
-        
+        private static bool mythicplusinterrupts
+        {
+            get
+            {
+                var mythicplusinterrupts = ConfigFile.ReadValue("ProtectionLesion", "mythicplusinterrupts").Trim();
+
+                return mythicplusinterrupts != "" && Convert.ToBoolean(mythicplusinterrupts);
+            }
+            set { ConfigFile.WriteValue("ProtectionLesion", "mythicplusinterrupts", value.ToString()); }
+        }
 		public override Form SettingsForm { get; set; }
 		
 		
@@ -90,20 +100,34 @@ namespace PixelMagic.Rotation
             MplusAoE = new CheckBox {Checked = MplusAoEint, TabIndex = 2, Size = new Size(15, 14), Left = 200, Top = 29};
             SettingsForm.Controls.Add(MplusAoE);
 			
+			var lblMythicPlusText = new Label //12; 129 is first value, Top is second.
+            {
+                Text = "M+ Interrupts",
+                Size = new Size(188, 13), //95; 13
+                Left = 12,
+                Top = 44
+            };
+            SettingsForm.Controls.Add(lblMythicPlusText); //113; 114 
+
+            mplusint = new CheckBox {Checked = mythicplusinterrupts, TabIndex = 4, Size = new Size(15, 14), Left = 200, Top = 44};
+            SettingsForm.Controls.Add(mplusint);
 			
 			
 			var cmdSave = new Button {Text = "Save", Width = 65, Height = 25, Left = 15, Top = 150, Size = new Size(120, 48)};
 			
 			TrueFrothingZerkerFalseMassacre.Checked = TFZFMint;
 			MplusAoE.Checked = MplusAoEint;
+			mplusint.Checked = mythicplusinterrupts;
 			
 			cmdSave.Click += CmdSave_Click;
 			TrueFrothingZerkerFalseMassacre.CheckedChanged += TFZFM_Click;
 			MplusAoE.CheckedChanged += MplusAoE_Click;
+            mplusint.CheckedChanged += MP_Click;
 			
 			SettingsForm.Controls.Add(cmdSave);
             lblTFZFMintText.BringToFront();
 			lblMplusAoEintText.BringToFront();
+			lblMythicPlusText.BringToFront();
 			
 			Log.Write("---------------------------------------------------------", Color.Blue);
             Log.Write("IF True = Frothing Zerk, IF False = Massacre = ", Color.Red);
@@ -113,6 +137,8 @@ namespace PixelMagic.Rotation
 			private void CmdSave_Click(object sender, EventArgs e)
         {
             TFZFMint = TrueFrothingZerkerFalseMassacre.Checked;
+			MplusAoEint = MplusAoE.Checked;
+            mythicplusinterrupts = mplusint.Checked;
             			
             MessageBox.Show("Settings saved", "PixelMagic", MessageBoxButtons.OK, MessageBoxIcon.Information);
             SettingsForm.Close();
@@ -127,7 +153,12 @@ namespace PixelMagic.Rotation
         {
             MplusAoEint = MplusAoE.Checked;
         }
-
+		
+		private void MP_Click(object sender, EventArgs e)
+        {
+            mythicplusinterrupts = mplusint.Checked;
+        }
+		
         public override void Stop()
         {
         }
@@ -161,7 +192,7 @@ namespace PixelMagic.Rotation
 							{WoW.CastSpell("Whirlwind"); return;}
 							
 							if (WoW.CanCast("Rampage")
-								&& WoW.Rage >= 100
+								&& WoW.IsSpellOverlayed("Rampage")
 								&& WoW.PlayerHasBuff("Meat Cleaver"))
 							{WoW.CastSpell("Rampage"); return;}
 							
@@ -251,7 +282,7 @@ namespace PixelMagic.Rotation
                         if (TFZFMint)
                         {
                             if (WoW.CanCast("Rampage")
-                                && WoW.Rage >= 100)
+                                && WoW.IsSpellOverlayed("Rampage") || WoW.Rage >= 100)
                             { WoW.CastSpell("Rampage"); return; }
 
                             if (WoW.CanCast("Bloodthirst")
@@ -290,7 +321,7 @@ namespace PixelMagic.Rotation
                             && WoW.IsSpellOverlayed("Execute"))
                         { WoW.CastSpell("Execute"); return; }
 
-                        if (!MplusAoEint && WoW.CanCast("Raging Blow")
+                        if (WoW.CanCast("Raging Blow")
                             && !WoW.CanCast("Bloodthirst"))
                         { WoW.CastSpell("Raging Blow"); return; }
 
@@ -311,7 +342,7 @@ namespace PixelMagic.Rotation
                         if (TFZFMint)
                         {
                             if (WoW.CanCast("Rampage")
-                                && WoW.Rage >= 100)
+                                && WoW.IsSpellOverlayed("Rampage") || WoW.Rage >= 100)
                             { WoW.CastSpell("Rampage"); return; }
 
                             if (WoW.CanCast("Bloodthirst")
@@ -327,7 +358,7 @@ namespace PixelMagic.Rotation
                                 && WoW.Rage > 25)
                             { WoW.CastSpell("Execute"); return; }
 
-                            if (!MplusAoEint && WoW.CanCast("Raging Blow")
+                            if (WoW.CanCast("Raging Blow")
                                 && !WoW.CanCast("Bloodthirst"))
                             { WoW.CastSpell("Raging Blow"); return; }
 
@@ -366,7 +397,59 @@ namespace PixelMagic.Rotation
 
                         }
                     }
+				
+						if (mythicplusinterrupts)
+                    {
+                        if (WoW.CanCast("Pummel") && WoW.TargetIsCasting && WoW.TargetIsCastingAndSpellIsInterruptible)
+                        {
+//int spell list for all important spells in M+                        
+                            if (WoW.TargetCastingSpellID == 200248
+//Court Of Stars Mythic+ Interrupt list
+                                || WoW.TargetCastingSpellID == 225573 || WoW.TargetCastingSpellID == 208165 || WoW.TargetCastingSpellID == 211401 || WoW.TargetCastingSpellID == 21147 ||
+                                WoW.TargetCastingSpellID == 211299 || WoW.TargetCastingSpellID == 2251 || WoW.TargetCastingSpellID == 209413 || WoW.TargetCastingSpellID == 209404 ||
+                                WoW.TargetCastingSpellID == 215204 || WoW.TargetCastingSpellID == 210261
+//Darkheart Thicket Mythic+ Interrupt list
+                                || WoW.TargetCastingSpellID == 200658 || WoW.TargetCastingSpellID == 200631 || WoW.TargetCastingSpellID == 204246 || WoW.TargetCastingSpellID == 2014
+//Eye of Azshara Mythic+ Interrupt list
+                                || WoW.TargetCastingSpellID == 19687 || WoW.TargetCastingSpellID == 218532 || WoW.TargetCastingSpellID == 195129 || WoW.TargetCastingSpellID == 195046 ||
+                                WoW.TargetCastingSpellID == 197502 || WoW.TargetCastingSpellID == 196027 || WoW.TargetCastingSpellID == 196175 || WoW.TargetCastingSpellID == 192003 ||
+                                WoW.TargetCastingSpellID == 191848
+//Halls of Valor Mythic+ Interrupt list
+                                || WoW.TargetCastingSpellID == 198595 || WoW.TargetCastingSpellID == 198962 || WoW.TargetCastingSpellID == 198931 || WoW.TargetCastingSpellID == 192563 ||
+                                WoW.TargetCastingSpellID == 192288 || WoW.TargetCastingSpellID == 199726
+//Maw of Souls Mythic+ Interrupt list
+                                || WoW.TargetCastingSpellID == 198495 || WoW.TargetCastingSpellID == 195293 || WoW.TargetCastingSpellID == 199589 || WoW.TargetCastingSpellID == 194266 ||
+                                WoW.TargetCastingSpellID == 198405 || WoW.TargetCastingSpellID == 199514 || WoW.TargetCastingSpellID == 194657
+//Neltharions Lair Mythic+ Interrupt list
+                                || WoW.TargetCastingSpellID == 193585 || WoW.TargetCastingSpellID == 202181
+//The Arcway Mythic+ Interrupt list
+                                || WoW.TargetCastingSpellID == 226269 || WoW.TargetCastingSpellID == 211007 || WoW.TargetCastingSpellID == 211757 || WoW.TargetCastingSpellID == 226285 ||
+                                WoW.TargetCastingSpellID == 226206 || WoW.TargetCastingSpellID == 211115 || WoW.TargetCastingSpellID == 196392
+// Advisor Vandros (Interrupt manually) Spell,203176,Accelerating Blast
+                                || WoW.TargetCastingSpellID == 203957
+//Vault of the Wardens Mythic+ Interrupt list
+                                || WoW.TargetCastingSpellID == 193069 || WoW.TargetCastingSpellID == 191823 || WoW.TargetCastingSpellID == 202661 || WoW.TargetCastingSpellID == 201488 ||
+                                WoW.TargetCastingSpellID == 195332
+//Raid Interrupts
+                                || WoW.TargetCastingSpellID == 209485 || WoW.TargetCastingSpellID == 209410 || WoW.TargetCastingSpellID == 211470 || WoW.TargetCastingSpellID == 225100 ||
+                                WoW.TargetCastingSpellID == 207980 || WoW.TargetCastingSpellID == 196870 || WoW.TargetCastingSpellID == 195284 || WoW.TargetCastingSpellID == 192005 ||
+                                WoW.TargetCastingSpellID == 228255 || WoW.TargetCastingSpellID == 228239 || WoW.TargetCastingSpellID == 227917 || WoW.TargetCastingSpellID == 228625 ||
+                                WoW.TargetCastingSpellID == 228606 || WoW.TargetCastingSpellID == 229714 || WoW.TargetCastingSpellID == 227592 || WoW.TargetCastingSpellID == 229083 ||
+                                WoW.TargetCastingSpellID == 228025 || WoW.TargetCastingSpellID == 228019 || WoW.TargetCastingSpellID == 227987 || WoW.TargetCastingSpellID == 227420 ||
+                                WoW.TargetCastingSpellID == 200905)
 
+                            {
+                                if (!WoW.IsSpellOnCooldown("Pummel") && WoW.TargetPercentCast >= 40)
+                                {
+                                    WoW.CastSpell("Pummel");
+                                    return;
+                                }
+							}
+				
+						}
+					}
+				
+				
                 }	
 			}
 				
